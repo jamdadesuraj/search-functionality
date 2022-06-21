01b6b5ec075fc9e7022e81f346df72d34b7d1c8b
unknown <surajjamdade83086@gmail.com>
2022-06-21 13:25:04 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Search from "./Search";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
